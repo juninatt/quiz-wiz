f79f4390c7a7ad072f2853f5dca3778a097084d8
Petter B <petter.bergstrom@protonmail.com>
2023-10-24 20:32:42 +0200

    Move necessary component scan for testing to TestDatabaseImplementation.java

--- src/test/java/se/juninatt/quizwiz/config/implementation/TestDatabaseImplementation.java
+++ src/test/java/se/juninatt/quizwiz/config/implementation/TestDatabaseImplementation.java
@@ -1,13 +1,19 @@
-package se.juninatt.quizwiz.config;
+package se.juninatt.quizwiz.config.implementation;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import se.juninatt.quizwiz.config.TestDataSourceConfig;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
+@ComponentScan(basePackages = {
+        "se.juninatt.quizwiz.service",
+        "se.juninatt.quizwiz.repository",
+})
 @Import(TestDataSourceConfig.class)
 public abstract class TestDatabaseImplementation {
 }
