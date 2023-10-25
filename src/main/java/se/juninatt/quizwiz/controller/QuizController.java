1a37ac25f7ba351b290a7e2564fad724b6a2db3d
Petter B <petter.bergstrom@protonmail.com>
2023-10-25 15:51:05 +0200

    Add html, css and script files for quiz creation

--- src/main/java/se/juninatt/quizwiz/controller/QuizController.java
+++ src/main/java/se/juninatt/quizwiz/controller/QuizController.java
@@ -1,13 +1,36 @@
 package se.juninatt.quizwiz.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import se.juninatt.quizwiz.model.entity.Quiz;
 
+/**
+ * Controller to manage {@link Quiz}-related routes and views.
+ */
 @Controller
 public class QuizController {
 
+    private static final Logger logger = LoggerFactory.getLogger(QuizController.class);
+
+    /**
+     * Show the welcome page of the quiz application.
+     * @param model The model object to pass attributes to the view.
+     * @return The name of the Thymeleaf template to render.
+     */
     @GetMapping("/quizwiz")
-    public String showQuiz() {
-        return "welcome";
+    public String showWelcomePage(Model model) {
+        logger.info("Showing welcome page.");
+        model.addAttribute("message", "    Welcome to the Quiz Portal. Choose from various quizzes or create your own!");
+        return "welcome-page";
+    }
+
+
+    @GetMapping("/create-quiz")
+    public String showCreateQuizPage(Model model) {
+        model.addAttribute("page_title", "Create Your Own Quiz");
+        return "create-quiz-page";
     }
 }
---
+++ src/main/resources/static/js/main-menu.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+     const menuButtons = document.querySelectorAll('.menu-button');
+
+     menuButtons.forEach(button => {
+         button.addEventListener('click', function() {
+             if (this.id === 'create-quiz-button') {
+                 window.location.href = '/create-quiz';
+             } else if (this.id === 'welcome-page-button') {
+                 window.location.href = '/quizwiz';
+             } else {
+                 alert('You clicked a menu button!');
+             }
+         });
+     });
+ });
